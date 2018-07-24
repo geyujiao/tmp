9cb51cae618600420e097aa5adf7ff09b51f593e
wangrui <228226928@qq.com>
2018-07-24 10:48:42 +0800

    delete

--- config/spcard.go
+++
@@ -1,9 +0,0 @@
-package config
-
-var Spcard spcard
-
-type spcard struct {
-	ChargeNumber string `config:"chargeNumber"`
-	ListenPort   string `config:"listenPort"`
-	ChargeType   string `config:"type"`
-}
