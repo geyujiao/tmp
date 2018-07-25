3b29b261fd5d5fefc5ad8c4d989e27e8849fc761
wangrui <228226928@qq.com>
2018-07-25 18:23:01 +0800

    update consumer

--- controller/result.go
+++ controller/result.go
@@ -4,10 +4,15 @@ import (
 	"encoding/json"
 	"log"
 
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/vgmdj/tmp/util/charge/proxy"
 	"github.com/vgmdj/tmp/util/mq/rabbitmq"
 	"github.com/vgmdj/utils/logger"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 var (
@@ -28,30 +33,13 @@ type MsgResult struct {
 //ResultStatistics 充值结果
 func ResultStatistics(c *gin.Context) {
 
-	logger.Info(c.Request.Body)
-	rechargeData := proxy.ParseData(c.Request.Body)
-	logger.Info(rechargeData)
-
-	result := MsgResult{
-		OrderNo: rechargeData.Orderid,
-		Money:   rechargeData.Amount,
-		Message: rechargeData.Msg,
-		ErrInfo: rechargeData.Msg,
-		//OilCard: "",
-		//Pwd:     "",
-	}
-	if rechargeData.Status == proxy.ResponseSuccess {
-		result.Result = ChargeMsgSuccess
-
-	} else if rechargeData.Status == proxy.ResponseFail {
-		result.Result = ChargeMsgFailed
-	} else {
-		logger.Error("invalid status")
+	result, err := parseData(c.Request.Body)
+	if err != nil {
+		logger.Error(err.Error())
 		return
-
 	}
 
-	err := sendMsgToResultQue(result)
+	err = sendMsgToResultQue(result)
 	if err != nil {
 		logger.Error(err.Error())
 		return
@@ -78,6 +66,62 @@ func sendMsgToResultQue(result MsgResult) (err error) {
 	if err != nil {
 		log.Println(err.Error())
 		return
+	}
+
+	return
+}
+
+func parseData(body io.Reader) (result MsgResult, err error) {
+	tmp, err := ioutil.ReadAll(body)
+	if err != nil {
+		logger.Error(err.Error())
+		return
+	}
+
+	values, err := url.ParseQuery(string(tmp))
+	if err != nil {
+		logger.Error(err.Error())
+		return
+	}
+
+	//Code        string    //状态码 0
+	//Status      string //订单状态
+	//Msg         string //订单状态说明
+	//Amount      string //实际金额
+	//Orderid     string //订单号
+	//OutOrderNo  string //官方流水号
+	//From_status string
+
+	tmpRes := make(map[string]string)
+	keys := []string{"code", "status", "msg", "amount", "Orderid", "outOrderNo", "from_status"}
+	for _, v := range keys {
+		var (
+			value []string
+			ok    bool
+		)
+		if value, ok = values[v]; !ok {
+			logger.Error("no such key", v, values)
+			continue
+		}
+
+		tmpRes[v] = value[0]
+	}
+
+	result = MsgResult{
+		OrderNo: tmpRes["Orderid"],
+		Money:   tmpRes["amount"],
+		Message: tmpRes["msg"],
+		ErrInfo: tmpRes["msg"],
+	}
+
+	if tmpRes["status"] == proxy.ResponseSuccess {
+		result.Result = ChargeMsgSuccess
+
+	} else if tmpRes["status"] == proxy.ResponseFail {
+		result.Result = ChargeMsgFailed
+
+	} else {
+		return result, fmt.Errorf("invalid status")
 	}
 
 	return
